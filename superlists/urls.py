35b6294cb17273c6392a16b87fcd9fc02e273edf
Michael Wittig <michael.wittig@fu-berlin.de>
2015-06-14 10:22:55 +0200

    staticfiles in development environment

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from lists import views as list_views
 from lists import urls as list_urls
+
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    staticfiles_urlpatterns(),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
