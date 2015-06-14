22e7218a0ebed2f7d07f5f0b1c87b52f3aaa9b58
Michael Wittig <michael.wittig@fu-berlin.de>
2015-06-14 10:25:58 +0200

    remove failing staticfiles

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import include, url
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from lists import views as list_views
 from lists import urls as list_urls
 
@@ -9,7 +7,6 @@ from lists import urls as list_urls
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
-    staticfiles_urlpatterns(),
     # url(r'^admin/', include(admin.site.urls)),
 ]
 
